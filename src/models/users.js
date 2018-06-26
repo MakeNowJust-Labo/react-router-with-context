355ba8dc30cb1d564400b611093649b24880c3a7
TSUYUSATO Kitsune <make.just.on@gmail.com>
2018-06-26 21:27:33 +0900

    feat: add 'update' method

--- src/models/users.js
+++ src/models/users.js
@@ -68,6 +68,22 @@ export class UsersProvider extends Component {
           this.setState({loading: false});
         }
       },
+      update: async (id, icon) => {
+        if (state.loading) {
+          return;
+        }
+
+        try {
+          this.setState({loading: true});
+          const {data: user} = await axios.post(`http://localhost:3000/user/${id}`, {icon});
+          const cache = Object.assign({}, this.state.cache, {[user.id]: user});
+          this.setState({cache});
+        } catch (error) {
+          this.setState({error});
+        } finally {
+          this.setState({loading: false});
+        }
+      },
     };
 
     return <UsersContext.Provider value={users}>{this.props.children}</UsersContext.Provider>;
